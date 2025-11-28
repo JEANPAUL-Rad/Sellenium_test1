36665cfe9dcab4319cf3058944969ff10275d507
jean paul IRADUKUNDA <uyuwe60@gmail.com>
2025-11-28 12:39:46 +0200

    click and forms works

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+
+    private By readMoreLink = By.linkText("Read More");
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void clickLinks(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+    }
+
+    public FormsPage clickEnrollYourself(){
+        driver.findElement(By.linkText("ENROLL YOURSELF")).click();
+        return new FormsPage(driver);
+    }
+
+    public SeleniumTrainingPage clickReadMore(){
+        driver.findElement(readMoreLink).click();
+        return new SeleniumTrainingPage(driver);
+    }
+}
---
+++ src/test/java/forms/FormTests.java
@@ -0,0 +1,22 @@
+package forms;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.FormsPage;
+
+public class FormTests extends BaseTests {
+    @Test
+    public void testEnrollYourselfLink(){
+        FormsPage formsPage = homePage.clickEnrollYourself();
+
+        formsPage.fillFirstName("Jean paul");
+        formsPage.fillLastName("Aman");
+        formsPage.fillEmail("musa@gmail.com");
+        formsPage.fillMobile("0780097403");
+        formsPage.selectCountry("Rwanda");
+        formsPage.fillCity("Kigali");
+        formsPage.fillMessage("This is a test message");
+        formsPage.fillCaptcha("3UOt");
+        formsPage.clickSend();
+    }
+}
